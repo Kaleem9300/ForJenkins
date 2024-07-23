e2893ebe0bfc5a749dc7239b44a43c68119494f6
Syed Kaleemuddin <syed_kaleemuddin@epam.com>
2024-07-23 11:16:25 +0530

    For Jenkins jobs

---
+++ src/main/java/com/jobs/JenkinTask.java
@@ -0,0 +1,12 @@
+package com.jobs;
+
+public class JenkinTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("To test jenkins jobs");
+		
+	}
+
+}
